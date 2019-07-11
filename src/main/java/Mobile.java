a1caffa6e315e1fbf96fb3b30c455afe538167fa
JERRY LI (ITA-ISDC-ISD-OOCLL/ZHA) <LIJE9@OOCL.COM>
2019-07-11 11:57:24 +0800

    finish story1-1 Mobile

---
+++ src/main/java/Mobile.java
@@ -0,0 +1,39 @@
+public class Mobile {
+    private String name;
+    private String color;
+    private  String brand;
+
+    public void call(String message){
+        System.out.println("Message : ["+message+"]");
+    }
+
+    public Mobile(String name, String color, String brand){
+        setName(name);
+        setColor(color);
+        setBrand(brand);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+}
